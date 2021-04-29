ac9d27df3dbdeb6219fc691628e7da9aa16fd40e
jdcho <lubiksss@gmail.com>
2021-04-29 19:41:16 +0900

    lab

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
 N = int(input())
-roads = list(map(int, input().split()))
-price = list(map(int, input().split()))
+listn = list(map(int,input().split()))
+listn = sorted(listn)
 
-flag = 0
-MIN = price[flag]*roads[0]
-
-for i in range(1, N-1):
-    if price[i] < price[flag]:
-        flag = i
-    MIN += price[flag]*roads[i]
-
-print(MIN)
+print(listn[0]*listn[-1])
