22736eda8c08b41b106d1e30d3010ad2b117b442
JAEDEOKJO <lubiksss@gmail.com>
2021-07-22 14:17:46 +0900

    10816_lower_upper_bound

--- 10816.py
+++ 10816.py
@@ -7,55 +7,27 @@ num_list = list(map(int, input().split()))
 m = int(input())
 num_list2 = list(map(int, input().split()))
 
+def lower_bound(start, end, target):
 
-def isinlist(x):
-
-    start = 0
-    end = len(num_list)-1
-
-    while True:
+    while end>start:
         mid = (start+end)//2
-        if x == num_list[mid]:
-            return mid
-
-        elif x > num_list[mid]:
-            if mid+1 > end:
-                return 500001
-            else:
-                start = mid+1
-
+        if num_list[mid] >= target:
+            end = mid
         else:
-            if mid-1 < start:
-                return 500001
-            else:
-                end = mid-1
+            start = mid+1
+    return end+1
+        
+def upper_bound(start, end, target):
 
+    while end>start:
+        mid = (start+end)//2
+        if num_list[mid] > target:
+            end = mid
+        else:
+            start = mid+1
+    return end+1
 
-def howmany(x, mid):
-    if mid == 500001:
-        return 0
-
-    else:
-        cnt = 1
-        lidx = 0
-        ridx = 0
-
-        while mid-lidx-1 >= 0 and num_list[mid-lidx-1] == x:
-            lidx += 1
-            cnt += 1
-
-        while mid+ridx+1 <= n-1 and num_list[mid+ridx+1] == x:
-            ridx += 1
-            cnt += 1
-
-        return cnt
-
-## 소팅함수가 nlogn
 num_list = sorted(num_list)
 
-print(num_list)
 for num in num_list2:
-    ##logn으로 탐색하고, 좌우로 넓히면서 확인, 최악의경우 n개다확인
-    ##그럼 nlong이고
-    ##근데 데이터의 갯수만큼 확인하니까 n* nlong인데.. 돌아갈까..
-    print(howmany(num, isinlist(num)), end=' ')
+    print(upper_bound(0,n, num) - lower_bound(0,n,num), end = ' ')
