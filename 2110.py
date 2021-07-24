d74ea66f2664968e8cb67423dbda55d1bb8eed05
JAEDEOKJO <lubiksss@gmail.com>
2021-07-24 14:44:50 +0900

    2110_binary_search

--- 2110.py
+++ 2110.py
@@ -7,16 +7,26 @@ num_list = [int(input()) for __ in range(n)]
 ## O(nlogn)
 num_list.sort()
 
-def ispossible():
-    pass
+def ispossible(mid):
+    tmp=1
+    start = 0
+    for i in range(len(num_list)):
+        if num_list[i] - num_list[start] >= mid:
+            tmp+=1
+            start = i
+    return tmp
+
+
+def cal_max(start, end):
+    tmp = -1
+    while start <= end:
+        mid= (start+end)//2
+        if ispossible(mid) >= c:
+            tmp = mid
+            start = mid+1
+        else:
+            end = mid-1
+    return tmp
 
 start, end = 1,num_list[-1]
-tmp = -1
-
-while start <= end:
-    mid= (start+end)//2
-    if ispossible(mid) >= c:
-        tmp = mid
-        start = mid+1
-    else:
-        end = mid-1
+print(cal_max(start,end))
