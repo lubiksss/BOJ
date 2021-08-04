8e12c20d757bced6c1b693031b3d04408adc536f
JAEDEOKJO <lubiksss@gmail.com>
2021-08-05 01:45:55 +0900

    update

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-a = 10
+import sys
+a = [1, 2, 3, 4]
+# b = [1, 2, 3, 4, 5]
+print(sys.getsizeof(a))
+print(id(a))
 
 
-def test():
-    a = 20
-    print(a)
+def test(a):
+    print(sys.getsizeof(a))
+    print(id(a))
 
 
-test()
-
-print(a)
+test(a)
