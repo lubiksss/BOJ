f3444e45dd07014d1c4afd4c2a0a64749a274b82
JAEDEOKJO <lubiksss@gmail.com>
2021-08-14 15:40:28 +0900

    update

--- 11404.py
+++ 11404.py
@@ -2,23 +2,33 @@ import sys
 input = sys.stdin.readline
 
 vertex_num, edge_num = int(input()), int(input())
-dist_list = [[sys.maxsize] * (vertex_num) for __ in range(vertex_num)]
 
-for i in range(vertex_num):
-    dist_list[i][i] = 0
 
-for i in range(edge_num):
-    start, end, weight = map(int, input().split())
-    if dist_list[start-1][end-1] == sys.maxsize:
-        dist_list[start-1][end-1] = weight
-    else:
-        dist_list[start-1][end-1] = min(weight, dist_list[start-1][end-1])
+def make_dist(vertex_num, edge_num):
+    dist_list = [[sys.maxsize] * (vertex_num) for __ in range(vertex_num)]
+    for i in range(vertex_num):
+        dist_list[i][i] = 0
 
-for mid in range(vertex_num):
-    for row in range(vertex_num):
-        for col in range(vertex_num):
-            dist_list[row][col] = min(
-                dist_list[row][col], dist_list[row][mid]+dist_list[mid][col])
+    for i in range(edge_num):
+        start, end, weight = map(int, input().split())
+        if dist_list[start-1][end-1] == sys.maxsize:
+            dist_list[start-1][end-1] = weight
+        else:
+            dist_list[start-1][end-1] = min(weight, dist_list[start-1][end-1])
+    return dist_list
+
+
+def Floyd_Warshall(vertex_num, dist_list):
+    for mid in range(vertex_num):
+        for row in range(vertex_num):
+            for col in range(vertex_num):
+                dist_list[row][col] = min(
+                    dist_list[row][col], dist_list[row][mid]+dist_list[mid][col])
+
+
+dist_list = make_dist(vertex_num, edge_num)
+Floyd_Warshall(vertex_num, dist_list)
+
 
 for row in dist_list:
     for dist in row:
--- 11657.py
+++ 11657.py
@@ -2,24 +2,34 @@ import sys
 input = sys.stdin.readline
 
 vertex_num, edge_num = map(int, input().split())
-edge_list = []
 dist_list = [sys.maxsize] * (vertex_num+1)
 dist_list[1] = 0
-negative_loop = []
 
-for i in range(edge_num):
-    start, end, weight = map(int, input().split())
-    edge_list.append([start, end, weight])
 
-for i in range(vertex_num):
-    for edge in edge_list:
-        start, end, weight = edge
-        if dist_list[start] != sys.maxsize:
-            dist_list[end] = min(dist_list[start]+weight,  dist_list[end])
-    if i == vertex_num-2:
-        negative_loop.append(dist_list[:])
-    if i == vertex_num-1:
-        negative_loop.append(dist_list[:])
+def make_edge():
+    edge_list = []
+    for i in range(edge_num):
+        start, end, weight = map(int, input().split())
+        edge_list.append([start, end, weight])
+    return edge_list
+
+
+def Bellman_Ford(edge_list):
+    negative_loop = []
+    for i in range(vertex_num):
+        for edge in edge_list:
+            start, end, weight = edge
+            if dist_list[start] != sys.maxsize:
+                dist_list[end] = min(dist_list[start]+weight,  dist_list[end])
+        if i == vertex_num-2:
+            negative_loop.append(dist_list[:])
+        if i == vertex_num-1:
+            negative_loop.append(dist_list[:])
+    return negative_loop
+
+
+edge_list = make_edge()
+negative_loop = Bellman_Ford(edge_list)
 
 if negative_loop[0] == negative_loop[1]:
     for i in negative_loop[1][2:]:
