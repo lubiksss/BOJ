c405135665150213ba11e87290bbb09d1d23c7f1
JAEDEOKJO <lubiksss@gmail.com>
2021-08-16 16:46:03 +0900

    update

--- 11404.py
+++ 11404.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import sys
 input = sys.stdin.readline
 
@@ -6,28 +7,31 @@ vertex_num, edge_num = int(input()), int(input())
 
 def make_dist(vertex_num, edge_num):
     dist_list = [[sys.maxsize] * (vertex_num) for __ in range(vertex_num)]
+    prev_list = [[-1] * (vertex_num) for __ in range(vertex_num)]
     for i in range(vertex_num):
         dist_list[i][i] = 0
 
     for i in range(edge_num):
         start, end, weight = map(int, input().split())
-        if dist_list[start-1][end-1] == sys.maxsize:
+        if weight < dist_list[start-1][end-1]:
             dist_list[start-1][end-1] = weight
-        else:
-            dist_list[start-1][end-1] = min(weight, dist_list[start-1][end-1])
-    return dist_list
+            prev_list[start-1][end-1] = start
+    return dist_list, prev_list
 
 
 def Floyd_Warshall(vertex_num, dist_list):
     for mid in range(vertex_num):
         for row in range(vertex_num):
             for col in range(vertex_num):
-                dist_list[row][col] = min(
-                    dist_list[row][col], dist_list[row][mid]+dist_list[mid][col])
+                if dist_list[row][col] > dist_list[row][mid]+dist_list[mid][col]:
+                    dist_list[row][col] = dist_list[row][mid] + \
+                        dist_list[mid][col]
+                    prev_list[row][col] = prev_list[mid][col]
 
 
-dist_list = make_dist(vertex_num, edge_num)
+dist_list, prev_list = make_dist(vertex_num, edge_num)
 Floyd_Warshall(vertex_num, dist_list)
+pprint(prev_list)
 
 
 for row in dist_list:
@@ -37,3 +41,19 @@ for row in dist_list:
         else:
             print(dist, end=' ')
     print()
+
+# prev_list에서 최단경로 뽑는 과정
+path_list = [[-1]*vertex_num for __ in range(vertex_num)]
+for row in range(vertex_num):
+    for col in range(vertex_num):
+        path_list[row][col] = []
+        path_list[row][col].append(col+1)
+        start = row
+        end = col
+        while start != end:
+            path_list[row][col].append(prev_list[start][end])
+            end = prev_list[start][end] - 1
+
+for i in range(vertex_num):
+    for j in range(vertex_num):
+        print(f'{i+1}=>{j+1}경로: {list(reversed(path_list[i][j]))}')
--- test.py
+++ test.py
@@ -1,15 +1,7 @@
-import numpy
-visited = [[0]*(3) for __ in range(3)]
-for i in range(3):
-    for j in range(3):
-        visited[i][j] = []
+import sys
 
+a = sys.maxsize
+b = sys.maxsize
 
-print(visited)
-
-visited[0][0].append(1)
-
-print(id(visited[0][0]))
-print(id(visited[0][1]))
-
-print(visited)
+if a*2 > b:
+    print(1)
