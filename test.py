652ee5cee4ac3257f80231e853dc32cebef551aa
JAEDEOKJO <lubiksss@gmail.com>
2021-09-23 22:54:16 +0900

    sort

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-a = {"a": 1, "b": 2}
+import sys
+import time
+import random
 
-print(len(a))
+NUM = 10
+lista = random.sample(range(NUM), NUM)
+print(lista)
+
+
+def bubble_sort(lista):
+    tmp = lista
+    for i in range(len(tmp)-1):
+        for j in range(len(tmp)-1-i):
+            if tmp[j] > tmp[j+1]:
+                tmp[j], tmp[j+1] = tmp[j+1], tmp[j]
+    return tmp
+
+
+print(bubble_sort(lista))
