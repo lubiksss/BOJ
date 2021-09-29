4a62a2a9fc473ed4829a944ff29a1a1d82f3388f
JAEDEOKJO <lubiksss@gmail.com>
2021-09-29 22:27:41 +0900

    MST

--- 17472.py
+++ 17472.py
@@ -3,8 +3,13 @@ import heapq
 input = sys.stdin.readline
 
 
+def pprint(map):
+    for row in map:
+        print(row)
+
+
 def find(a):
-    if a == parent[a]:
+    if parent[a] == a:
         return a
     parent[a] = find(parent[a])
     return parent[a]
@@ -18,28 +23,92 @@ def union(a, b):
     parent[b] = a
 
 
-N = int(input())
-v = []
-heap = []
-parent = [i for i in range(N)]
+N, M = map(int, input().split())
+map = [list(map(int, input().split())) for __ in range(N)]
+visited = [[False]*M for __ in range(N)]
+
+idx = 1
+edge_list = []
+que = []
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
+
+for y in range(N):
+    for x in range(M):
+        if map[y][x] == 0:
+            continue
+        if visited[y][x]:
+            continue
+
+        que.append([y, x])
+        visited[y][x] = True
+
+        while que:
+            basey, basex = que.pop()
+            map[basey][basex] = idx
+            for i in range(4):
+                nexty = basey + dy[i]
+                nextx = basex + dx[i]
+                if 0 <= nexty < N and 0 <= nextx < M and map[nexty][nextx] != 0 and not visited[nexty][nextx]:
+                    que.append([nexty, nextx])
+                    visited[nexty][nextx] = True
+
+        idx += 1
+
+for y in range(N):
+    for x in range(M):
+        if map[y][x] == 0:
+            continue
+        # 12시 방향.
+        for i in range(1, N):
+            nexty = y-i
+            nextx = x
+            if not (0 <= nexty < N) or map[nexty][nextx] == map[y][x]:
+                break
+            if map[nexty][nextx] != 0:
+                heapq.heappush(edge_list, [i-1, map[y][x], map[nexty][nextx]])
+                break
+        # 3시 방향.
+        for i in range(1, M):
+            nexty = y
+            nextx = x+i
+            if not (0 <= nextx < M) or map[nexty][nextx] == map[y][x]:
+                break
+            if map[nexty][nextx] != 0:
+                heapq.heappush(edge_list, [i-1, map[y][x], map[nexty][nextx]])
+                break
+        # 6시 방향.
+        for i in range(1, N):
+            nexty = y+i
+            nextx = x
+            if not (0 <= nexty < N) or map[nexty][nextx] == map[y][x]:
+                break
+            if map[nexty][nextx] != 0:
+                heapq.heappush(edge_list, [i-1, map[y][x], map[nexty][nextx]])
+                break
+        # 9시 방향.
+        for i in range(1, M):
+            nexty = y
+            nextx = x-i
+            if not (0 <= nextx < M) or map[nexty][nextx] == map[y][x]:
+                break
+            if map[nexty][nextx] != 0:
+                heapq.heappush(edge_list, [i-1, map[y][x], map[nexty][nextx]])
+                break
+
+
+parent = [i for i in range(idx+1)]
 wsum = 0
+cnt = 0
 
-for i in range(N):
-    x, y, z = map(int, input().split())
-    v.append([x, y, z, i])
-
-# 엄청난 아이디어의 부분
-for i in range(3):
-    v.sort(key=lambda x: x[i])
-    for j in range(1, N):
-        heapq.heappush(
-            heap, (abs(v[j - 1][i] - v[j][i]), v[j - 1][3], v[j][3]))
-
-while heap:
-    w, a, b = heapq.heappop(heap)
-    if find(a) == find(b):
+while edge_list:
+    w, s, e = heapq.heappop(edge_list)
+    if w == 1:
         continue
-    union(a, b)
+    if find(s) == find(e):
+        continue
+    union(s, e)
     wsum += w
+    cnt += 1
 
-print(wsum)
+print(wsum if cnt == idx-2 else -1)
