fa32a0039e48877ff7a3b081a1d9396f855d2f98
JAEDEOKJO <lubiksss@gmail.com>
2021-10-15 22:20:34 +0900

    dnq

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-from typing import Counter
-a = [1, 2, 1, 0, 0, 0, 1]
-
-
-def count(list):
-    cnt = 0
-    for e in list:
-        if e != 0:
-            cnt += 1
-    return cnt
-
-
-print(count(a))
+print(2**3**2)
