d793fee0e4cacdf9646361798d0fab4877ca6300
JAEDEOKJO <lubiksss@gmail.com>
2021-11-05 17:00:52 +0900

    update

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-parent = [i for i in range(1, 11)]
+a = '1'
+b = '2'
 
 
-def find(a):
-    if parent[a] == a:
-        return
-    parent[a] = find(parent[a])
-    return parent[a]
-
-
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a == b:
-        return
-    parent[b] = a
+print(a <= b)
